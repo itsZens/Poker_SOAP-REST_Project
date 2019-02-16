bfd7fa5926b6fd95aad06a84e202993b13ec550a
itsZens <danimr1999@gmail.com>
2019-02-16 18:28:26 +0100

    Comments deleted

--- Rest.js
+++ Rest.js
@@ -361,33 +361,6 @@ app.put('/tirarCarta/codiPartida/:carta', (req, res)=>{
     res.send(baraja);
     
 });
-/*
-app.get('/api/alumnes', (req, res)=>res.send(alumnes));
-app.get('/api/alumnes/:codi', (req, res)=>{
-    var alumne = alumnes.find(a =>a.codi===parseInt(req.params.codi));
-    if (!alumne) res.status(404, 'error');
-    res.send(alumne);
-});
 
-app.post('/api/alumnes', (req, res)=>{
-    console.log('req.body.codi');
-    var alumne={codi: req.body.codi, nom: req.body.nom };
-    alumnes.push(alumne);
-    res.send(alumnes);
-});
-
-app.delete('/api/alumnes/:codi', (req, res)=>{
-    var alumne = alumnes.find(a =>a.codi===parseInt(req.params.codi));
-    var index =alumnes.indexOf(alumne);
-    alumnes.splice(index, 1);
-    res.send();
-});
-app.put('/api/alumnes/:codi', (req, res)=>{
-    var nouAlumne={codi: req.body.codi, nom: req.body.nom };
-    var alumne = alumnes.find(a =>a.codi===parseInt(req.params.codi));
-    var index =alumnes.indexOf(alumne);
-    alumnes[index]=nouAlumne;
-});
-*/
 app.listen(3000, ()=>console.log('inici servidor'));
 
